6245d14d40956ffa5c20cdd41629decc111144c5
Lee Lup Yuen <luppy@appkaki.com>
2024-02-27 20:24:54 +0800

    Show disassembly

--- docs/purescript/disassemble.js
+++ docs/purescript/disassemble.js
@@ -1,8 +1,9 @@
-const addr = "80007028";
+// Show the NuttX Disassembly for the Requested Address
+// http://localhost:8000/nuttx-tinyemu/docs/purescript/disassemble.html?addr=80007028
 
-// Show 10 lines before and after the Requested Address
-const before_count = 10;
-const after_count = 10;
+// Show 20 lines before and after the Requested Address
+const before_count = 20;
+const after_count  = 20;
 
 // Fetch our Disassembly File, line by line
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#processing_a_text_file_line_by_line
@@ -39,8 +40,13 @@ async function* makeTextFileLineIterator(fileURL) {
 
 // Fetch and display our Disassembly File, line by line
 async function run() {
-  
-  // URL of our Disassembly File
+
+  // `addr` is `80007028`
+  const addr = new URL(document.URL).searchParams.get("addr");
+
+  // URL of our Disassembly File, chunked for easier display.
+  // TODO: Given an Exception Address like 8000ad8a. we should try multiple files by address:
+  // qjs-8000ad90.S, qjs-8000ae00.S, qjs-8000b000.S, qjs-80010000.S
   const url = "qjs-chunk/qjs-80008000.S";
 
   // Remember the lines before and after the Requested Address
@@ -70,7 +76,13 @@ async function run() {
     }
   }
 
+  // Requested Line is `after_lines[0]`.
+  // Show the Before and After Lines.
+  const line = after_lines[0];
+  after_lines.shift();
+
   console.log({before_lines});
+  console.log({line});
   console.log({after_lines});
 }
 
